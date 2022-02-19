df2b8482d41318c2e47faf5bdadf7268d3bc24f4
Leo <aammlj2002@gmail.com>
2022-02-19 16:37:46 +0630

    added design integrations

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 app.listen(3000);
 app.get("/", (req, res) => {
-    res.send("<h1>hello express</h1>");
+    res.sendFile("./views/index.html", { root: __dirname });
 });
 app.get("/add-items", (req, res) => {
-    res.send("<h1>add items</h1>");
+    res.sendFile("./views/add-items.html", { root: __dirname });
+});
+app.use((req, res) => {
+    res.sendFile("./views/404.html", { root: __dirname });
 });
